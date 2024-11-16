a006a21c96d1c1cc003ac1cbeedf79ce191e5830
handoko <srabutdotcom@gmail.com>
2024-11-17 00:41:16 +0700

    add @type in test file

--- test/encryptedext_test.js
+++ test/encryptedext_test.js
@@ -1,8 +1,10 @@
 import { ExtensionType } from "jsr:@tls/enum";
 import { EncryptedExtensions } from "../src/encryptedext.js";
 import { assertEquals } from "jsr:@std/assert";
+import { Extension } from "../src/dep.js";
 
 Deno.test("EncryptedExtension", ()=>{
+   //@type {Extension} extension - description
    const extension = ExtensionType.KEY_SHARE.extension(Uint8Array.of(1,4,6))
    const test = EncryptedExtensions.fromExtensions(extension);
    const back = EncryptedExtensions.from(test)
