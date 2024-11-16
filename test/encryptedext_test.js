3f3764631315dd553a83cb741612f362a71cac23
handoko <srabutdotcom@gmail.com>
2024-11-17 00:22:45 +0700

    add Extension to encryptedext_test.js

--- test/encryptedext_test.js
+++ test/encryptedext_test.js
@@ -1,6 +1,7 @@
 import { ExtensionType } from "@tls/enum";
 import { EncryptedExtensions } from "../src/encryptedext.js";
 import { assertEquals } from "jsr:@std/assert";
+import { Extension } from "../src/dep.ts";
 
 Deno.test("EncryptedExtension", ()=>{
    const test = EncryptedExtensions.fromExtensions(ExtensionType.KEY_SHARE.extension(Uint8Array.of(1,4,6)));
