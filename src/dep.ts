37e90207ac359bd7236c45f468de25dfffea8a8f
handoko <srabutdotcom@gmail.com>
2024-11-16 23:52:56 +0700

    ver 0.0.4 delete jsr:

--- src/dep.ts
+++ src/dep.ts
@@ -1,2 +1,2 @@
-export { Struct, Constrained, Byte, Uint8, Uint16, Uint24, Uint32 } from "jsr:@tls/struct"
+export { Struct, Constrained, Byte, Uint8, Uint16, Uint24, Uint32 } from "@tls/struct"
 export { Extension } from "@tls/extension"
