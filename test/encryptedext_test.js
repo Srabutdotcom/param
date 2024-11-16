444d79e5d2ad8ecb3e544cc6a66a13c0db8dfdff
handoko <srabutdotcom@gmail.com>
2024-11-17 00:36:49 +0700

    add extension in test file

--- test/encryptedext_test.js
+++ test/encryptedext_test.js
@@ -3,7 +3,8 @@ import { EncryptedExtensions } from "../src/encryptedext.js";
 import { assertEquals } from "jsr:@std/assert";
 
 Deno.test("EncryptedExtension", ()=>{
-   const test = EncryptedExtensions.fromExtensions(ExtensionType.KEY_SHARE.extension(Uint8Array.of(1,4,6)));
+   const extension = ExtensionType.KEY_SHARE.extension(Uint8Array.of(1,4,6))
+   const test = EncryptedExtensions.fromExtensions(extension);
    const back = EncryptedExtensions.from(test)
    assertEquals(test, back)
 })
